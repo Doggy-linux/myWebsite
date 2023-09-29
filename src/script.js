c0057975a9a747b55e81f6031b46b87e1a64556c
Doggy-linux <133614143+Doggy-linux@users.noreply.github.com>
2023-09-29 12:05:26 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+alert("Hello!");
+
+function myYoutube() {
+    location.replace("https://www.youtube.com/@doggylinux");
+}
+function answerWhatLanguage() {
+    var favoriteLanguage = prompt("What is your favorite programming language?");
+    console.log(favoriteLanguage);
+    alert(favoriteLanguage + ", huh... That's nice! I like Javascript!")
+}
+function myFavYoutuber() {
+    location.replace("https://www.youtube.com/@NetworkChuck");
+}
+function favoriteGame() {
+    var whatIsYourFavoriteGame = prompt("What is your favorite game?");
+    console.log(whatIsYourFavoriteGame);
+    alert(whatIsYourFavoriteGame + "? That sounds fun!");
+}
